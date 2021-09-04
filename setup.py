6c3140cd45d5f97acc31c01756511efb72cae5e0
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-04 17:02:23 +0200

    created requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 
+import re
+
+with open('requirements.txt', 'r') as file:
+    requirements = [re.sub('\n','',i) for i in file]
+
 setup(
     name='sqlIntuitive',
     package_dir={"":"src"},
@@ -8,7 +13,7 @@ setup(
     description='Library for easy and intuitive use of database systems.',
     author="Moritz Bauer",
     license="MIT",
-    install_requires=[],
+    install_requires=requirements,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests'
