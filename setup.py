f0a1976bee8689ba7115f3f8929831a98d5c7d52
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-07 17:43:23 +0200

    global | Version 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='sqlIntuitive',
     package_dir={"":"src"},
     packages=find_packages(where="src"),
-    version='0.1.0',
+    version='0.2.0',
     description='Library for easy and intuitive use of database systems.',
     author="Moritz Bauer",
     license="MIT",
