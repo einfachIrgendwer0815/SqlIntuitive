c30944f037a5e0513b237a47e3a533d6843e2793
einfachIrgendwer0815 <eavmb@gmx.de>
2021-09-09 17:42:09 +0200

    dbSystems | Added MySqlDbSystem.connect_to_db(), MySqlDbSystem.close_connection(); ~ with test-cases

--- src/sqlIntuitive/dbSystems.py
+++ src/sqlIntuitive/dbSystems.py
@@ -1,5 +1,40 @@
+from mysql.connector import connection
+from mysql.connector import Error
+
+from time import sleep
+
 class SqliteDbSystem():
     pass
 
 class MySqlDbSystem():
-    pass
+    def __init__(self, host, database, username, password, max_connect_retries=5):
+        self.host = host
+        self.database = database
+        self.username = username
+        self.password = password
+
+        self.dbCon = None
+        self.max_connect_retries = max_connect_retries
+
+    def connect_to_db(self, retryNo=0, timeout=3):
+        if self.dbCon != None: return False
+
+        try:
+            self.dbCon = connection.MySQLConnection(
+                host=self.host,
+                database=self.database,
+                username=self.username,
+                password=self.password
+            )
+        except Error as e:
+            print(e)
+            sleep(timeout)
+
+            if retryNo >= self.max_connect_retries or self.connect_to_db(retryNo+1) == False:
+                return False
+
+        return True
+
+    def close_connection(self):
+        if self.dbCon != None and self.dbCon.is_connected():
+            self.dbCon.close()
---
+++ tests/test_dbSystems.py
@@ -0,0 +1,42 @@
+from sqlIntuitive import dbSystems
+
+import pytest
+
+import json
+
+with open('tests/mysql_testserver_login.json', 'r') as file:
+    mysql_login = json.load(file)
+
+def test_A_connect_to_db__close_connection():
+    mydb = dbSystems.MySqlDbSystem(
+        host=mysql_login["host"],
+        database=mysql_login["database"],
+        username=mysql_login["username"],
+        password=mysql_login["password"],
+    )
+
+    assert mydb.connect_to_db() == True
+    assert mydb.dbCon != None
+    assert mydb.dbCon.is_connected()
+
+    mydb.close_connection()
+
+    assert mydb.dbCon.is_connected() == False
+
+def test_B_close_connection():
+    mydb = dbSystems.MySqlDbSystem(
+        host=mysql_login["host"],
+        database=mysql_login["database"],
+        username=mysql_login["username"],
+        password=mysql_login["password"],
+    )
+
+    assert mydb.dbCon == None
+
+    mydb.close_connection()
+
+    assert mydb.connect_to_db() == True
+
+    mydb.close_connection()
+
+    mydb.close_connection() # No close happens, but no error should appear.
