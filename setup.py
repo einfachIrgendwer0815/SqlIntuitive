1aa08441b40b4eaba1e7b7f4f59489d7606836b1
einfachIrgendwer0815 <eavmb@gmx.de>
2021-10-14 17:16:05 +0200

    global | Added reading version from file

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import find_packages, setup
 
 import re
+import json
 
 with open('requirements.txt', 'r') as file:
     requirements = [re.sub('\n','',i) for i in file]
+
+with open('version.json', 'r') as file:
+    version = json.load(file)['version']
 
 setup(
     name='sqlIntuitive',
     python_requires='>=3.8',
     package_dir={"":"src"},
     packages=find_packages(where="src"),
-    version='0.4.2',
+    version=version,
     description='Library for easy and intuitive use of database systems.',
     author="Moritz Bauer",
     license="MIT",
