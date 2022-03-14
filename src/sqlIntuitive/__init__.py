b60b8604eb43a3c6a1e7d534abd7fd414107e9f3
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-03-14 18:08:57 +0100

    version | Version bump to 0.9.0

--- src/sqlIntuitive/__init__.py
+++ src/sqlIntuitive/__init__.py
@@ -1,5 +1,5 @@
 from sqlIntuitive import ext, dbSystems, exceptions, sqlGeneration
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 __author__ = "Moritz Bauer (einfachIrgendwer0815)"
 __license__ = "MIT"
