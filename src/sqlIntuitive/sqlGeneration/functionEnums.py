d6851b88ebd6b20e1690e3d261d5cbc6c697d2c5
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-03-23 15:19:32 +0100

    functionEnums | Changed functionEnums to map the operations to their responsible functions

--- src/sqlIntuitive/sqlGeneration/functionEnums.py
+++ src/sqlIntuitive/sqlGeneration/functionEnums.py
@@ -1,33 +1,31 @@
 from enum import Enum, auto
+from sqlIntuitive.sqlGeneration import standard, mysql, sqlite
 
-class NameAsValue(Enum):
-    def _generate_next_value_(name, start, count, last_values):
-        return name
+class Standard(Enum):
+    SELECT_FROM = standard.gen_select
+    SELECT_COUNT = standard.gen_count
+    SELECT_AVG = standard.gen_avg
+    SELECT_SUM = standard.gen_sum
+    SELECT_JOIN = standard.gen_select_join
+    INSERT_INTO = standard.gen_insert
+    UPDATE = standard.gen_update
+    DELETE_FROM = standard.gen_delete
+    CREATE_DB = standard.gen_create_db
+    DROP_DB = standard.gen_drop_db
+    CREATE_TABLE = standard.gen_create_table
+    DROP_TABLE = standard.gen_drop_table
+    ALTER_TABLE_ADD = standard.gen_alter_table_add
+    ALTER_TABLE_DROP = standard.gen_alter_table_drop
+    ALTER_TABLE_MODIFY = standard.gen_alter_table_modify
+    CREATE_STORED_PROCEDURE = standard.gen_create_stored_procedure
+    EXEC_PROCEDURE = standard.gen_exec_procedure
+    DROP_PROCEDURE = standard.gen_drop_procedure
 
-class Standard(NameAsValue):
-    SELECT_FROM = auto()
-    SELECT_COUNT = auto()
-    SELECT_AVG = auto()
-    SELECT_SUM = auto()
-    SELECT_JOIN = auto()
-    INSERT_INTO = auto()
-    UPDATE = auto()
-    DELETE_FROM = auto()
-    CREATE_DB = auto()
-    DROP_DB = auto()
-    CREATE_TABLE = auto()
-    DROP_TABLE = auto()
-    ALTER_TABLE_ADD = auto()
-    ALTER_TABLE_DROP = auto()
-    ALTER_TABLE_MODIFY = auto()
-    CREATE_STORED_PROCEDURE = auto()
-    EXEC_PROCEDURE = auto()
-    DROP_PROCEDURE = auto()
+class MySQL(Standard):
+    CREATE_STORED_PROCEDURE = mysql.gen_create_stored_procedure
+    EXEC_PROCEDURE = mysql.gen_exec_procedure
+    ALTER_TABLE_MODIFY = mysql.gen_alter_table_modify
 
-class MySQL(NameAsValue):
-    CREATE_STORED_PROCEDURE = auto()
-    EXEC_PROCEDURE = auto()
-    ALTER_TABLE_MODIFY = auto()
-
-class SqLite(NameAsValue):
-    ALTER_TABLE_RENAME = auto()
+class SqLite(Standard):
+    ALTER_TABLE_RENAME_TABLE = sqlite.gen_alter_table_rename_table
+    ALTER_TABLE_RENAME_COLUMN = sqlite.gen_alter_table_rename_column
