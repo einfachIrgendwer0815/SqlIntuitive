0e495ed8b68a131d5778ab1bf14122962678bbf6
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-03-25 16:46:20 +0100

    functionEnums | Changed import to fix name conflicts

--- src/sqlIntuitive/sqlGeneration/functionEnums.py
+++ src/sqlIntuitive/sqlGeneration/functionEnums.py
@@ -1,31 +1,31 @@
 from enum import Enum, auto
-from sqlIntuitive.sqlGeneration import standard, mysql, sqlite
+from sqlIntuitive import sqlGeneration
 
 class Standard(Enum):
-    SELECT_FROM = standard.gen_select
-    SELECT_COUNT = standard.gen_count
-    SELECT_AVG = standard.gen_avg
-    SELECT_SUM = standard.gen_sum
-    SELECT_JOIN = standard.gen_select_join
-    INSERT_INTO = standard.gen_insert
-    UPDATE = standard.gen_update
-    DELETE_FROM = standard.gen_delete
-    CREATE_DB = standard.gen_create_db
-    DROP_DB = standard.gen_drop_db
-    CREATE_TABLE = standard.gen_create_table
-    DROP_TABLE = standard.gen_drop_table
-    ALTER_TABLE_ADD = standard.gen_alter_table_add
-    ALTER_TABLE_DROP = standard.gen_alter_table_drop
-    ALTER_TABLE_MODIFY = standard.gen_alter_table_modify
-    CREATE_STORED_PROCEDURE = standard.gen_create_stored_procedure
-    EXEC_PROCEDURE = standard.gen_exec_procedure
-    DROP_PROCEDURE = standard.gen_drop_procedure
+    SELECT_FROM = sqlGeneration.standard.gen_select
+    SELECT_COUNT = sqlGeneration.standard.gen_count
+    SELECT_AVG = sqlGeneration.standard.gen_avg
+    SELECT_SUM = sqlGeneration.standard.gen_sum
+    SELECT_JOIN = sqlGeneration.standard.gen_select_join
+    INSERT_INTO = sqlGeneration.standard.gen_insert
+    UPDATE = sqlGeneration.standard.gen_update
+    DELETE_FROM = sqlGeneration.standard.gen_delete
+    CREATE_DB = sqlGeneration.standard.gen_create_db
+    DROP_DB = sqlGeneration.standard.gen_drop_db
+    CREATE_TABLE = sqlGeneration.standard.gen_create_table
+    DROP_TABLE = sqlGeneration.standard.gen_drop_table
+    ALTER_TABLE_ADD = sqlGeneration.standard.gen_alter_table_add
+    ALTER_TABLE_DROP = sqlGeneration.standard.gen_alter_table_drop
+    ALTER_TABLE_MODIFY = sqlGeneration.standard.gen_alter_table_modify
+    CREATE_STORED_PROCEDURE = sqlGeneration.standard.gen_create_stored_procedure
+    EXEC_PROCEDURE = sqlGeneration.standard.gen_exec_procedure
+    DROP_PROCEDURE = sqlGeneration.standard.gen_drop_procedure
 
 class MySQL(Standard):
-    CREATE_STORED_PROCEDURE = mysql.gen_create_stored_procedure
-    EXEC_PROCEDURE = mysql.gen_exec_procedure
-    ALTER_TABLE_MODIFY = mysql.gen_alter_table_modify
+    CREATE_STORED_PROCEDURE = sqlGeneration.mysql.gen_create_stored_procedure
+    EXEC_PROCEDURE = sqlGeneration.mysql.gen_exec_procedure
+    ALTER_TABLE_MODIFY = sqlGeneration.mysql.gen_alter_table_modify
 
 class SqLite(Standard):
-    ALTER_TABLE_RENAME_TABLE = sqlite.gen_alter_table_rename_table
-    ALTER_TABLE_RENAME_COLUMN = sqlite.gen_alter_table_rename_column
+    ALTER_TABLE_RENAME_TABLE = sqlGeneration.sqlite.gen_alter_table_rename_table
+    ALTER_TABLE_RENAME_COLUMN = sqlGeneration.sqlite.gen_alter_table_rename_column
